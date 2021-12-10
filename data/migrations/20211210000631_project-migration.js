16c1a50e46d386a36ac2f9a34b1446aeac5ef289
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2021-12-09 19:27:04 -0500

    migrations complete

---
+++ data/migrations/20211210000631_project-migration.js
@@ -0,0 +1,55 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('projects', (table) => {
+      table.increments('project_id').primary();
+      table.string('project_name').notNullable();
+      table.string('project_description');
+      table.boolean('project_completed').defaultTo(false);
+    })
+    .createTable('resources', (table) => {
+      table.increments('resource_id').primary();
+      table.string('resource_name').unique().notNullable();
+      table.string('resource_description');
+    })
+    .createTable('tasks', (table) => {
+      table.increments('task_id').primary();
+      table.string('task_description').notNullable();
+      table.string('task_notes');
+      table.boolean('task_completed').defaultTo(false);
+      table
+        .integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('project_id')
+        .inTable('projects')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT');
+    })
+    .createTable('project_resources', (table) => {
+      table.increments('project_resource_id').primary();
+      table
+        .integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('project_id')
+        .inTable('projects')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT');
+      table
+        .integer('resource_id')
+        .unsigned()
+        .notNullable()
+        .references('resource_id')
+        .inTable('resources')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT');
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('project_resources')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('projects');
+};

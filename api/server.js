5fc3f1ac8dd9f047777a2b863a3baf7cb0603ee7
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2021-12-09 19:04:52 -0500

    init setup

--- api/server.js
+++ api/server.js
@@ -1,1 +1,15 @@
-// build your server here and require it from index.js
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+//eslint-disable-next-line
+server.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
